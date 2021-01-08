dfda7901fd0a77fab19969dd4fd0a2f0e4997bfc
Alexander Matlin <ratingsoft@gmail.com>
2021-01-08 17:24:46 +0100

    Express.js Middleware.

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+const helmet = require('helmet');
+const morgan = require('morgan');
 const Joi = require('joi');
+const logger = require('./logger');
 const express = require('express');
-const { valid } = require('joi');
 const app = express();
 
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(helmet());
+
+if (app.get('env') === 'development') {
+	app.use(morgan('tiny'));
+	console.log('Morgan enabled...');
+}
+
+app.use(logger);
 
 let genres = [
 	{ id: 1, name: 'Genre 1' },
