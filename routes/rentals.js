02a4bb6703d9983231ae71279abf9734250f4d22
Aleksandr Matlin <ratingsoft@gmail.com>
2021-02-23 16:52:18 +0100

    Add Fawn for transaction on rentals.

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,8 +1,12 @@
 const { Rental, validate } = require('../models/rental');
 const { Movie } = require('../models/movie');
 const { Customer } = require('../models/customer');
+const mongoose = require('mongoose');
+const Fawn = require('fawn');
 const express = require('express');
-const router = require('router');
+const router = express.Router();
+
+Fawn.init(mongoose);
 
 router.get('/', async (req, res) => {
 	const rentals = await Rental.find().sort('-dateOut');
@@ -34,10 +38,17 @@ router.post('/', async (req, res) => {
 		}
 	});
 
-	rental = await rental.save();
-
-	movie.numberInStock--;
-	movie.save();
+	try {
+		new Fawn.Task()
+			.save('rentals', rental)
+			.update('movies', { _id: movie._id }, {
+				$inc: { numberInStock: -1 }
+			})
+			.run();
+	}
+	catch (ex) {
+		res.status(500).send('Something failed.');
+	}
 
 	res.send(rental);
 });
