f18a2095e38fd3966972bec7bc7c1c153b2e1b35
Aleksandr Matlin <ratingsoft@gmail.com>
2021-02-23 16:20:25 +0100

    Add the Rentals API.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const home = require('./routes/home');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 const express = require('express');
 const app = express();
 
@@ -20,6 +21,7 @@ app.use('/', home);
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
