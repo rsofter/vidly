9821c90d048141ca91427dd20e865923aee27030
Aleksandr Matlin <ratingsoft@gmail.com>
2021-02-25 11:20:09 +0100

    Add Authentication and Authorization.

--- index.js
+++ index.js
@@ -7,8 +7,15 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
+
+if (!config.get('jwtPrivateKey')) {
+	console.error('FATAL ERROR: jwtPrivateKey is not defined.');
+	process.exit(1);
+}
 
 mongoose.connect(config.get('database.mongodb'), {
 	useNewUrlParser: true,
@@ -24,6 +31,8 @@ app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- routes/movies.js
+++ routes/movies.js
@@ -1,3 +1,4 @@
+const auth = require('../middleware/auth');
 const { Movie, validate } = require('../models/movie');
 const { Genre } = require('../models/genre');
 const express = require('express');
@@ -10,7 +11,7 @@ router.get('/', async (req, res) => {
 });
 
 // Creating a new movie
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
 	const { error } = validate(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
@@ -34,7 +35,7 @@ router.post('/', async (req, res) => {
 });
 
 // Updating a movie
-router.put('/:id', async (req, res) => {
+router.put('/:id', auth, async (req, res) => {
 	const { error } = validate(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
@@ -62,7 +63,7 @@ router.put('/:id', async (req, res) => {
 });
 
 // Deleting a movie
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', auth, async (req, res) => {
 	const movie = await Movie.findByIdAndDelete(req.params.id);
 
 	if (!movie) return res.status(404).send('The movie with the given ID was not found.');
