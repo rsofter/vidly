788cbdf91245ef586b10bc092ae515311f0dbdb4
Aleksandr Matlin <ratingsoft@gmail.com>
2021-02-27 17:57:33 +0100

    Add errors handling and loging with MW and winston.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+require('express-async-errors');
+const winston = require('winston');
+require('winston-mongodb');
+const error = require('./middleware/error');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
@@ -11,6 +15,19 @@ const users = require('./routes/users');
 const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
+
+process.on('uncaughtException', (ex) => {
+	winston.error(ex.message, { metadata: ex });
+	process.exit(1);
+});
+
+process.on('unhandledRejection', (ex) => {
+	winston.error(ex.message, ex);
+	process.exit(1);
+});
+
+winston.add(new winston.transports.File({ filename: 'logs/logfile.log' }));
+winston.add(new winston.transports.MongoDB({ db: config.get('database.mongodb') }));
 
 if (!config.get('jwtPrivateKey')) {
 	console.error('FATAL ERROR: jwtPrivateKey is not defined.');
@@ -33,6 +50,8 @@ app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
+
+app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
