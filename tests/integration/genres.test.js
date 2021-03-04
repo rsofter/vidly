688ffe686f3d16a5e65f080bdc407250318b0877
Aleksandr Matlin <ratingsoft@gmail.com>
2021-03-04 22:59:34 +0100

    Add tests for auth middleware.

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,13 +1,13 @@
 const request = require('supertest');
-const mongoose = require('mongoose');
 const { Genre } = require('../../models/genre');
+const { User } = require('../../models/user');
 let server;
 
 describe('/api/genres', () => {
 	beforeEach(() => { server = require('../../index'); });
 	afterEach(async () => {
-		server.close();
 		await Genre.remove({});
+		server.close();
 	});
 
 	describe('GET /', () => {
@@ -41,6 +41,62 @@ describe('/api/genres', () => {
 			const res = await request(server).get('/api/genres/1');
 
 			expect(res.status).toBe(404);
+		});
+	});
+
+	describe('POST /', () => {
+		let token;
+		let name;
+
+		const exec = async () => (
+			await request(server)
+				.post('/api/genres')
+				.set('x-auth-token', token)
+				.send({ name })
+		);
+
+		beforeEach(() => {
+			token = new User().generateAuthToken();
+			name = 'genre1';
+		});
+
+		it('should return 401 if client is not logged in', async () => {
+			token = '';
+
+			const res = await exec();
+
+			expect(res.status).toBe(401);
+		});
+
+		it('should return 400 if genre is less than 5 characters', async () => {
+			name = '1234';
+
+			const res = await exec();
+
+			expect(res.status).toBe(400);
+		});
+
+		it('should return 400 if genre is more than 50 characters', async () => {
+			name = new Array(52).join('a');
+
+			const res = await exec();
+
+			expect(res.status).toBe(400);
+		});
+
+		it('should save the genre if it is valid', async () => {
+			await exec();
+
+			const genre = await Genre.find({ name: 'genre1' });
+
+			expect(genre).not.toBeNull();
+		});
+
+		it('should save the genre if it is valid', async () => {
+			const res = await exec();
+
+			expect(res.body).toHaveProperty('_id');
+			expect(res.body).toHaveProperty('name', 'genre1');
 		});
 	});
 });
