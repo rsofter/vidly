1bbf14e86c6f2f0186dce319653a1097e39b21fe
Aleksandr Matlin <ratingsoft@gmail.com>
2021-03-09 23:57:18 +0100

    Add Rental instance method to make calculation on return.

--- routes/returns.js
+++ routes/returns.js
@@ -1,5 +1,4 @@
 const Joi = require('joi');
-const moment = require('moment');
 const validate = require('../middleware/validate');
 const auth = require('../middleware/auth');
 const { Rental } = require('../models/rental');
@@ -14,16 +13,14 @@ router.post('/', [auth, validate(validateReturn)], async (req, res) => {
 
 	if (rental.dateReturned) return res.status(400).send('Rental already processed');
 
-	rental.dateReturned = new Date();
-	const rentalDays = moment().diff(rental.dateOut, 'days');
-	rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
+	rental.return();
 	await rental.save();
 
 	await Movie.updateOne({ _id: rental.movie._id }, {
 		$inc: { numberInStock: 1 }
 	});
 
-	return res.status(200).send(rental);
+	return res.send(rental);
 });
 
 function validateReturn(req) {
