1c5d43a85a41aabc7b17f522969cf6887aa95f1b
Aleksandr Matlin <ratingsoft@gmail.com>
2021-03-09 23:45:56 +0100

    Add a validate middleware.

--- routes/returns.js
+++ routes/returns.js
@@ -1,14 +1,13 @@
+const Joi = require('joi');
 const moment = require('moment');
+const validate = require('../middleware/validate');
 const auth = require('../middleware/auth');
 const { Rental } = require('../models/rental');
 const { Movie } = require('../models/movie');
 const express = require('express');
 const router = express.Router();
 
-router.post('/', auth, async (req, res) => {
-	if (!req.body.customerId) return res.status(400).send('customerId is not provided');
-	if (!req.body.movieId) return res.status(400).send('movieId is not provided');
-
+router.post('/', [auth, validate(validateReturn)], async (req, res) => {
 	const rental = await Rental.findOne({
 		'customer._id': req.body.customerId,
 		'movie._id': req.body.movieId
@@ -28,5 +27,14 @@ router.post('/', auth, async (req, res) => {
 
 	return res.status(200).send(rental);
 });
+
+function validateReturn(req) {
+	const schema = Joi.object({
+		customerId: Joi.objectId().required(),
+		movieId: Joi.objectId().required()
+	});
+
+	return schema.validate(req);
+}
 
 module.exports = router;
